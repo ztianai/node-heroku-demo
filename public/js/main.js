315584b669d52ab9e814cda6c1d3217267e77cd2
Trevor Allen <tallen94@uw.edu>
2015-11-23 12:48:22 -0800

    reques using https not htto

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 
 angular.module('my-app', [])
 
-.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
-	$http.get('http://heroku-node-demo-info343.herokuapp.com/api')
+.controller('MainCtrl', ['$scope', '$https', function($scope, $https) {
+	$https.get('http://heroku-node-demo-info343.herokuapp.com/api')
 			.then(function(response) {
 				console.log(response.data);
 				$scope.server = response.data;
