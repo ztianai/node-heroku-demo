09ee1ecc72e8728cfca3039af5a04c447fc86b96
Trevor Allen <tallen94@uw.edu>
2015-11-22 18:59:58 -0800

    reverts changes

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var express = require('express');
-var http = require('http');
 var app = express();
 
-http.createServer(app.listen(8080));
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
 	res.send("hello there...");
 });
 
-app.listen = function() {
-	var server = http.createServer.(this);
-	return server.listen.apply(server, arguments);
-}
+app.listen(port, function() {
+	console.log('Our app is running on port: ' + port);
+});
