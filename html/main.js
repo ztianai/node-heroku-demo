835019b965f3213428bb841a8aa1c6c8fabe63dd
Trevor Allen <tallen94@uw.edu>
2015-11-22 18:48:24 -0800

    removes port from get request

--- html/main.js
+++ html/main.js
@@ -2,7 +2,7 @@
 angular.module('my-app', [])
 
 .controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
-	$http.get('http://heroku-node-demo-info343.herokuapp.com:8080/')
+	$http.get('http://heroku-node-demo-info343.herokuapp.com/')
 			.then(function(response) {
 				console.log("Response: " + repsonse);
 				$scope.message(response);
