5384aa9d4abefb3c89a1b268e7664d7ab38dae31
Trevor Allen <tallen94@uw.edu>
2015-11-22 19:17:03 -0800

    Allows access control headers

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
