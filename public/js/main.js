4a64dcdccc245dbb479535a91b20d32489d2795e
Trevor Allen <tallen94@uw.edu>
2015-11-23 12:51:09 -0800

    reques using https not http

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 
 angular.module('my-app', [])
 
-.controller('MainCtrl', ['$scope', '$https', function($scope, $http) {
+.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 	$http.get('https://heroku-node-demo-info343.herokuapp.com/api')
 			.then(function(response) {
 				console.log(response.data);
