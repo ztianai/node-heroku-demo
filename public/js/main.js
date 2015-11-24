c39f49371286689dbbb57f529fa24d8c51ec3adb
Trevor Allen <tallen94@uw.edu>
2015-11-23 20:02:53 -0800

    Start of tutorial

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,5 @@
 angular.module('my-app', [])
 
 .controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
-	$http.get('https://heroku-node-demo-info343.herokuapp.com/api/')
-			.then(function(response) {
-				console.log(response.data);
-				$scope.server = response.data;
-			}
-	);
+	
 }])
